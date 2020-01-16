d713aac7052a2e9f89eb64d1747abb6aec455df1
James Jeon <jkh911208@gmail.com>
2020-01-16 14:28:39 -0600

    add logging

--- main.py
+++ main.py
@@ -3,9 +3,23 @@
 from flask import Flask
 app = Flask(__name__)
 
+import logging                                                                  
+import logging.handlers                                                         
+logger = logging.getLogger("alert-connector")                                                    
+sh = logging.handlers.SysLogHandler(address=("localhost", 514), facility="local0")                 
+formatter =  logging.Formatter('%(name)s:%(message)s')                          
+sh.setFormatter(formatter)                                                      
+logger.addHandler(sh)                                                           
+
+
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
+
+@app.route("/logging")
+def logging():
+    logger.error("test")
+    return "log written"
 
 
 if __name__ == '__main__':
